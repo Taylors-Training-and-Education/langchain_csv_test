a6214bcae18b7db276bb0ae17daf33c5495a86a0
Taylor Erwin <90368987+TMoneyBidness@users.noreply.github.com>
2023-06-05 11:29:03 -0400

    Update agent.py

--- agent.py
+++ agent.py
@@ -43,41 +43,45 @@ def query_agent(agent, query):
 
     prompt = (
         """
-            For the following query, if it requires drawing a table, reply as follows:
-            {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}}
+        For the following query, if it requires drawing a table, reply as follows:
+        {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}}
 
-            If the query requires creating a bar chart, reply as follows:
-            {"bar": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
-            
-            If the query requires creating a line chart, reply as follows:
-            {"line": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
-            
-            There can only be two types of chart, "bar" and "line".
-            
-            If it is just asking a question that requires neither, reply as follows:
-            {"answer": "answer"}
-            Example:
-            {"answer": "The title with the highest rating is 'Gilead'"}
-            
-            If you do not know the answer, reply as follows:
-            {"answer": "I do not know."}
-            
-            Return all output as a string.
-            
-            All strings in "columns" list and data list, should be in double quotes,
-            
-            For example: {"columns": ["title", "ratings_count"], "data": [["Gilead", 361], ["Spider's Web", 5164]]}
-            
-            Lets think step by step.
-            
-            Below is the query.
-            Query: 
-            """
+        If the query requires creating a bar chart, reply as follows:
+        {"bar": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
+
+        If the query requires creating a line chart, reply as follows:
+        {"line": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
+
+        There can only be two types of chart, "bar" and "line".
+
+        If it is just asking a question that requires neither, reply as follows:
+        {"answer": "answer"}
+        Example:
+        {"answer": "The title with the highest rating is 'Gilead'"}
+
+        If you do not know the answer, reply as follows:
+        {"answer": "I do not know."}
+
+        Return all output as a string.
+
+        All strings in "columns" list and data list, should be in double quotes,
+
+        For example: {"columns": ["title", "ratings_count"], "data": [["Gilead", 361], ["Spider's Web", 5164]]}
+
+        Let's think step by step.
+
+        Below is the query.
+        Query:
+        """
         + query
     )
 
-    # Run the prompt through the agent.
-    response = agent.run(prompt)
+    try:
+        # Run the prompt through the agent.
+        response = agent.run(prompt)
+    except openai.error.InvalidRequestError as e:
+        # Handle the case when the prompt length exceeds the maximum token limit
+        response = str(e)
 
     # Convert the response to a string.
-    return response.__str__()
+    return str(response)
